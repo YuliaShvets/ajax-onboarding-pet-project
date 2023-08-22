f94885e0d18e25ccabc2fb3dd018d7d15daacb49
YuliaShvets <yulia.shvets04@gmail.com>
2023-08-22 13:50:33 +0300

    added kotlin files to handle exceptions

---
+++ src/main/java/ua/lviv/iot/parkingServer/logic/exception/EntityNotFoundException.kt
@@ -0,0 +1,4 @@
+package ua.lviv.iot.parkingServer.logic.exception
+
+class EntityNotFoundException(message: String) : RuntimeException(message) {
+}
---
+++ src/main/java/ua/lviv/iot/parkingServer/logic/exception/message/ErrorMessageModel.kt
@@ -0,0 +1,8 @@
+package ua.lviv.iot.parkingServer.logic.exception.message
+
+class ErrorMessageModel(
+    var status: Int? = null,
+    var message: String? = null
+) {
+
+}
