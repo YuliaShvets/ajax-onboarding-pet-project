b40e15d7f19f8c4d9ee3f8648e0a2fec9b736227
YuliaShvets <yulia.shvets04@gmail.com>
2023-08-25 15:17:50 +0300

    Fixed imports for PostConstruct and PreDestroy

--- src/main/java/ua/lviv/iot/parkingServer/logic/ParkingService.kt
+++ src/main/java/ua/lviv/iot/parkingServer/logic/ParkingService.kt
@@ -1,12 +1,12 @@
 package ua.lviv.iot.parkingServer.logic
 
+import jakarta.annotation.PostConstruct
+import jakarta.annotation.PreDestroy
 import java.util.concurrent.ConcurrentHashMap
 import org.springframework.stereotype.Service
 import ua.lviv.iot.parkingServer.datastorage.ParkingFileStore
 import ua.lviv.iot.parkingServer.model.Parking
 import java.util.concurrent.atomic.AtomicLong
-import javax.annotation.PostConstruct
-import javax.annotation.PreDestroy
 import ua.lviv.iot.parkingServer.logic.exception.EntityNotFoundException
 
 @Service
