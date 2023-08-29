0690d1faa2d31f908647e44d3ba5e655e62005a3
YuliaShvets <yulia.shvets04@gmail.com>
2023-08-29 16:46:30 +0300

    Removed unused imports

--- src/main/java/ua/lviv/iot/parkingServer/ParkingServerApplication.kt
+++ src/main/java/ua/lviv/iot/parkingServer/ParkingServerApplication.kt
@@ -2,8 +2,6 @@ package ua.lviv.iot.parkingServer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.EnableAspectJAutoProxy
 
 @SpringBootApplication
 class ParkingServerApplication
