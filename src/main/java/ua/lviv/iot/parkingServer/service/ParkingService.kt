8b005a249f9d23563d0b99f3d026de85b257d1f9
YuliaShvets <yulia.shvets04@gmail.com>
2023-09-04 16:39:21 +0300

    Moved to MongoRepository methods

--- src/main/java/ua/lviv/iot/parkingServer/service/ParkingService.kt
+++ src/main/java/ua/lviv/iot/parkingServer/service/ParkingService.kt
@@ -1,50 +1,28 @@
 package ua.lviv.iot.parkingServer.service
 
-import jakarta.annotation.PostConstruct
-import jakarta.annotation.PreDestroy
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.atomic.AtomicLong
 import org.springframework.stereotype.Service
 import ua.lviv.iot.parkingServer.annotation.TrackMetrics
+import ua.lviv.iot.parkingServer.repository.ParkingMongoRepository
 import ua.lviv.iot.parkingServer.exception.EntityNotFoundException
 import ua.lviv.iot.parkingServer.model.Parking
 import ua.lviv.iot.parkingServer.service.interfaces.ParkingServiceInterface
 
 @TrackMetrics
 @Service
-class ParkingService(private val parkingFileStore: ParkingFileStore) : ParkingServiceInterface {
-    private val parking: MutableMap<Long, Parking> = ConcurrentHashMap()
-    private var id: AtomicLong = AtomicLong(1L)
+class ParkingService(private val parkingMongoRepository: ParkingMongoRepository) : ParkingServiceInterface {
+    override fun findAllEntities(): List<Parking> = parkingMongoRepository.findAll()
 
-    override fun findAllEntities(): List<Parking> = parking.values.toList()
+    override fun findEntityById(id: Long): Parking = parkingMongoRepository.findById(id)
+        .orElseThrow { EntityNotFoundException("Parking with id=$id not found") }
 
-    override fun findEntityById(id: Long): Parking =
-        parking[id] ?: throw EntityNotFoundException("Parking with id=$id not found")
-
-    override fun addEntity(entity: Parking): Parking {
-        val newId = id.getAndIncrement()
-        entity.id = newId
-        parking[newId] = entity
-        return entity
-    }
+    override fun addEntity(entity: Parking): Parking = parkingMongoRepository.save(entity)
 
     override fun updateEntity(id: Long, entity: Parking): Parking {
-        entity.id = id
-        parking[id] = entity
-        return entity
+        entity.id = findEntityById(id).id
+        addEntity(entity)
+        return addEntity(entity)
     }
 
-    override fun deleteEntity(id: Long): Parking =
-        parking.remove(id) ?: throw EntityNotFoundException("Parking with id=$id doesn't exist")
-
-    @PostConstruct
-    override fun entityDataToHashMap() {
-        parkingFileStore.loadDataToHashmap(parkingFileStore, parking, id)
-    }
-
-    @PreDestroy
-    override fun saveEntityData() {
-        parkingFileStore.saveDataToFile(parkingFileStore, parking)
-    }
+    override fun deleteEntity(id: Long) = parkingMongoRepository.deleteById(id)
 
 }
--- src/main/java/ua/lviv/iot/parkingServer/service/VehicleService.kt
+++ src/main/java/ua/lviv/iot/parkingServer/service/VehicleService.kt
@@ -1,48 +1,26 @@
 package ua.lviv.iot.parkingServer.service
 
-import jakarta.annotation.PostConstruct
-import jakarta.annotation.PreDestroy
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.atomic.AtomicLong
 import org.springframework.stereotype.Service
+import ua.lviv.iot.parkingServer.repository.VehicleMongoRepository
 import ua.lviv.iot.parkingServer.exception.EntityNotFoundException
 import ua.lviv.iot.parkingServer.model.Vehicle
 import ua.lviv.iot.parkingServer.service.interfaces.VehicleServiceInterface
 
 @Service
-class VehicleService(private val vehicleFileStore: VehicleFileStore) : VehicleServiceInterface {
-    private val vehicles: MutableMap<Long, Vehicle> = ConcurrentHashMap()
-    private var id: AtomicLong = AtomicLong(1L)
-
-    override fun findAllEntities(): List<Vehicle> = vehicles.values.toList()
+class VehicleService(private val vehicleMongoRepository: VehicleMongoRepository) : VehicleServiceInterface {
+    override fun findAllEntities(): List<Vehicle> = vehicleMongoRepository.findAll()
 
     override fun findEntityById(id: Long): Vehicle =
-        vehicles[id] ?: throw EntityNotFoundException("Vehicle with id=$id not found")
+        vehicleMongoRepository.findById(id).orElseThrow { EntityNotFoundException("Vehicle with id=$id not found") }
 
-    override fun addEntity(entity: Vehicle): Vehicle {
-        val newId = id.getAndIncrement()
-        entity.id = newId
-        vehicles[newId] = entity
-        return entity
-    }
+    override fun addEntity(entity: Vehicle): Vehicle = vehicleMongoRepository.save(entity)
 
     override fun updateEntity(id: Long, entity: Vehicle): Vehicle {
-        entity.id = id
-        vehicles[id] = entity
-        return entity
+        entity.id = findEntityById(id).id
+        addEntity(entity)
+        return addEntity(entity)
     }
 
-    override fun deleteEntity(id: Long): Vehicle =
-        vehicles.remove(id) ?: throw EntityNotFoundException("Vehicle with id=$id doesn't exist")
-
-    @PostConstruct
-    override fun entityDataToHashMap() {
-        vehicleFileStore.loadDataToHashmap(vehicleFileStore, vehicles, id)
-    }
-
-    @PreDestroy
-    override fun saveEntityData() {
-        vehicleFileStore.saveDataToFile(vehicleFileStore, vehicles)
-    }
+    override fun deleteEntity(id: Long) = vehicleMongoRepository.deleteById(id)
 
 }
