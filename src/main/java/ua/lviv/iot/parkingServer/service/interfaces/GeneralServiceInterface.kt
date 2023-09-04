ca512e490a2c739d4083dc417c47496f2a1b7a62
YuliaShvets <yulia.shvets04@gmail.com>
2023-09-04 16:37:11 +0300

    Deleted redundant methods

--- src/main/java/ua/lviv/iot/parkingServer/service/interfaces/GeneralServiceInterface.kt
+++ src/main/java/ua/lviv/iot/parkingServer/service/interfaces/GeneralServiceInterface.kt
@@ -2,18 +2,14 @@ package ua.lviv.iot.parkingServer.service.interfaces
 
 interface GeneralServiceInterface<T, ID> {
 
-    fun findAllEntities() : List<T>
+    fun findAllEntities(): List<T>
 
-    fun findEntityById(id : Long) : T
+    fun findEntityById(id: Long): T
 
-    fun addEntity(entity : T) : T
+    fun addEntity(entity: T): T
 
-    fun updateEntity(id: Long, entity: T) : T
+    fun updateEntity(id: Long, entity: T): T
 
-    fun  deleteEntity(id: Long) : T
-
-    fun saveEntityData()
-
-    fun entityDataToHashMap()
+    fun deleteEntity(id: Long)
 
 }
