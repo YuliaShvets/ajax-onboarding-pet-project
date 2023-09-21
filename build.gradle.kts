6d91f6cb00a1a1157cf7b2232a38772feae8e5af
YuliaShvets <yulia.shvets04@gmail.com>
2023-09-21 11:08:56 +0300

    Fixed dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation(project(mapOf("path" to ":nats")))
+    implementation(project(":nats"))
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
