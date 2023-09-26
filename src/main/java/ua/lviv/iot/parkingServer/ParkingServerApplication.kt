039db85a370ceae374487a5f997b20484d240d70
YuliaShvets <yulia.shvets04@gmail.com>
2023-09-26 17:35:45 +0300

    Configured WebFlux

--- src/main/java/ua/lviv/iot/parkingServer/ParkingServerApplication.kt
+++ src/main/java/ua/lviv/iot/parkingServer/ParkingServerApplication.kt
@@ -2,7 +2,9 @@ package ua.lviv.iot.parkingServer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.reactive.config.EnableWebFlux
 
+@EnableWebFlux
 @SpringBootApplication
 class ParkingServerApplication
 
