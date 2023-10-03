53f70bde8a0079dd2c1526e082636c40b9dc38ce
YuliaShvets <yulia.shvets04@gmail.com>
2023-10-03 10:23:58 +0300

    Added test for reactive grpc services

---
+++ src/test/java/ua/lviv/iot/parkingServer/grpcservice/ReactorVehicleGrpcServiceTest.kt
@@ -0,0 +1,168 @@
+package ua.lviv.iot.parkingServer.grpcservice
+
+import io.grpc.ManagedChannel
+import io.grpc.ManagedChannelBuilder
+import java.time.Duration
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.SpringBootTest
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+import ua.lviv.iot.ReactorVehicleGrpcServiceGrpc
+import ua.lviv.iot.ReactorVehicleGrpcServiceGrpc.ReactorVehicleGrpcServiceStub
+import ua.lviv.iot.VehicleOuterClass
+import ua.lviv.iot.VehicleOuterClass.DeleteVehicleRequest
+import ua.lviv.iot.VehicleOuterClass.GetByIdVehicleRequest
+import ua.lviv.iot.VehicleOuterClass.UpdateVehicleRequest
+import ua.lviv.iot.VehicleOuterClass.VehicleRequest
+import ua.lviv.iot.VehicleOuterClass.VehicleResponse
+import ua.lviv.iot.parkingServer.converter.VehicleConverter
+import ua.lviv.iot.parkingServer.model.Vehicle
+import ua.lviv.iot.parkingServer.model.enums.VehicleType
+import ua.lviv.iot.parkingServer.repository.VehicleRepository
+
+@SpringBootTest
+class ReactorVehicleGrpcServiceTest(
+    @Value("\${grpc.server.port}")
+    var grpcPort: Int
+) {
+
+    @Autowired
+    private lateinit var vehicleConverter: VehicleConverter
+
+    @Autowired
+    private lateinit var vehicleRepository: VehicleRepository
+
+    private lateinit var stub: ReactorVehicleGrpcServiceStub
+
+    private lateinit var channel: ManagedChannel
+
+    private lateinit var firstTestVehicle: Vehicle
+
+    private lateinit var secondTestVehicle: Vehicle
+
+
+    @BeforeEach
+    fun setUp() {
+        firstTestVehicle = Vehicle(
+            number = "KA6706VN",
+            typeOfVehicle = VehicleType.BUS,
+            durationOfUseOfParkingSpot = Duration.ofSeconds(3600),
+            isTicketReceived = true
+        )
+        secondTestVehicle = Vehicle(
+            number = "KA6706VW",
+            typeOfVehicle = VehicleType.BUS,
+            durationOfUseOfParkingSpot = Duration.ofSeconds(3600),
+            isTicketReceived = true
+        )
+    }
+
+    @BeforeEach
+    fun start() {
+        channel = ManagedChannelBuilder
+            .forTarget("localhost:$grpcPort")
+            .usePlaintext()
+            .build()
+        stub = ReactorVehicleGrpcServiceGrpc.newReactorStub(channel)
+    }
+
+    @Test
+    fun createVehicle() {
+        val expected = VehicleResponse.newBuilder()
+            .setVehicle(vehicleConverter.vehicleToProto(firstTestVehicle))
+            .build()
+
+        val request = Mono.just(
+            VehicleRequest.newBuilder()
+                .setVehicle(vehicleConverter.vehicleToProto(firstTestVehicle))
+                .build()
+        )
+        val actual = stub.createVehicle(request)
+
+        val vehicleId = firstTestVehicle.id
+
+        StepVerifier.create(actual)
+            .assertNext {
+                assertThat(expected.vehicle).isEqualTo(it.vehicle)
+            }
+            .verifyComplete()
+        vehicleRepository.deleteById(vehicleId).block()
+    }
+
+
+    @Test
+    fun getVehicleById() {
+        vehicleRepository.save(firstTestVehicle).block()
+        val expected = VehicleResponse.newBuilder()
+            .setVehicle(vehicleConverter.vehicleToProto(firstTestVehicle))
+            .build()
+
+        val request = Mono.just(
+            GetByIdVehicleRequest.newBuilder()
+                .setVehicleId(firstTestVehicle.id)
+                .build()
+        )
+
+        val actual = stub.getVehicleById(request)
+
+        StepVerifier.create(actual)
+            .assertNext {
+                assertThat(expected.vehicle.number).isEqualTo(it.vehicle.number)
+            }
+            .verifyComplete()
+        vehicleRepository.deleteById(firstTestVehicle.id).block()
+    }
+
+    @Test
+    fun updateVehicle() {
+        val addedVehicle = vehicleRepository.save(secondTestVehicle).block()!!
+        firstTestVehicle.id = addedVehicle.id
+
+        val vehicle = vehicleConverter
+            .vehicleToProto(firstTestVehicle)
+
+        val expected =
+            VehicleResponse
+                .newBuilder()
+                .setVehicle(vehicle)
+                .build()
+
+        val request =
+            Mono.just(
+                UpdateVehicleRequest.newBuilder()
+                    .setVehicle(vehicle)
+                    .setVehicleId(addedVehicle.id)
+                    .build()
+            )
+        val actual =
+            stub.updateVehicle(request)
+
+        StepVerifier.create(actual)
+            .assertNext {
+                assertThat(expected.vehicle).isEqualTo(it.vehicle)
+            }
+            .verifyComplete()
+        vehicleRepository.deleteById(addedVehicle.id).block()
+    }
+
+    @Test
+    fun deleteVehicle() {
+        vehicleRepository.save(secondTestVehicle).block()
+
+        val request = Mono.just(
+            DeleteVehicleRequest.newBuilder()
+                .setVehicleId(secondTestVehicle.id)
+                .build()
+        )
+
+        val actual = stub.deleteVehicle(request)
+
+        StepVerifier.create(actual)
+            .expectNext(VehicleOuterClass.DeleteVehicleResponse.getDefaultInstance())
+            .verifyComplete()
+    }
+}
